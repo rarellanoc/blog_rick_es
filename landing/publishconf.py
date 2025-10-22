acfed7a4a49db7f4d2b2396ea83d295d2959eb51
Ricardo Arellano <developer@ricardoarellano.cl>
2025-10-22 07:54:44 -0300

    Change SITEURL and add FAVICON setting
    Updated SITEURL and added FAVICON configuration.

--- landing/publishconf.py
+++ landing/publishconf.py
@@ -7,8 +7,9 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://ricardoarellano.cl'
+SITEURL = 'blog-es.ricardoarellanoc.workers.dev'
 RELATIVE_URLS = False
+FAVICON = 'favicon.ico'
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
